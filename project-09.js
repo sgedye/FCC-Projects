5756c59253b249a8c8f74404891565ec9bea2cf0
Shaun Gedye <sgedye88@yahoo.com.au>
2019-10-25 13:51:59 +0800

    Create project-09.js

---
+++ project-09.js
@@ -0,0 +1,29 @@
+
+
+/* Return true if the passed string looks like a valid US phone number.
+
+The user may fill out the form field any way they choose as long as it has the format of a valid US number. The following are examples of valid formats for US numbers (refer to the tests below for other variants):
+
+555-555-5555
+(555)555-5555
+(555) 555-5555
+555 555 5555
+5555555555
+1 555 555 5555*/
+
+function telephoneCheck(str) {
+  // Check all conditions (with or without a country code)
+  let regex = /(?=^1?\s?\(?\d{3}\)?\s?-?\d{3}\s?-?\d{4}$)/;
+  let result = regex.test(str);
+  // Note, this regex does not check for both brackets
+  // Check if one bracket exists, but not the other
+  if ((str.indexOf('(') !== -1) && (str.indexOf(')') === -1)) {
+    result = false;
+  }
+  if ((str.indexOf('(') === -1) && (str.indexOf(')') !== -1)) {
+    result = false;
+  }
+  console.log(result);
+  return result;
+}
+telephoneCheck("(555)-555-5555");
