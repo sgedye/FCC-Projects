0af70d3bb41523a3a7303993b15e8d5e6076467b
Shaun Gedye <sgedye88@yahoo.com.au>
2019-10-25 14:07:20 +0800

    Update project-09.js

--- project-09.js
+++ project-09.js
@@ -1,16 +1,17 @@
-
+/*** Project 09 : Javascript Project 4 - Telephone Number Validator: ***/
 
 /* Return true if the passed string looks like a valid US phone number.
+The user may fill out the form field any way they choose as long as it has the format of a valid US number. 
+The following are examples of valid formats for US numbers (refer to the tests for other variants):
+  555-555-5555
+  (555)555-5555
+  (555) 555-5555
+  555 555 5555
+  5555555555
+  1 555 555 5555
+*/
 
-The user may fill out the form field any way they choose as long as it has the format of a valid US number. The following are examples of valid formats for US numbers (refer to the tests below for other variants):
-
-555-555-5555
-(555)555-5555
-(555) 555-5555
-555 555 5555
-5555555555
-1 555 555 5555*/
-
+// My solution
 function telephoneCheck(str) {
   // Check all conditions (with or without a country code)
   let regex = /(?=^1?\s?\(?\d{3}\)?\s?-?\d{3}\s?-?\d{4}$)/;
@@ -27,3 +28,10 @@ function telephoneCheck(str) {
   return result;
 }
 telephoneCheck("(555)-555-5555");
+
+// This can also be done without the if statements:
+function telephoneCheck(str) {
+  let regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+  return regex.test(str);
+}
+telephoneCheck("555-555-5555");
