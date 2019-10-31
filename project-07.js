82e8d4cdb834618f629fc4b9b1f4ddc055b492e4
Shaun Gedye <sgedye88@yahoo.com.au>
2019-10-31 16:01:57 +0800

    Update project-07.js

--- project-07.js
+++ project-07.js
@@ -1,74 +1,72 @@
 /*** Project 07 : Javascript Project 2 - Roman Numeral Converter: ***/
 
-let answer = "";
-let number = Math.floor(Math.random() * 4000);
+function convertToRoman(num) {
+  let answer = "";
+  while (num > 0) {
+    switch(true) {
+      case num > 3999:
+	answer = "Please enter a number between 1-3999";
+	num = 0;
+	break;
+      case num >= 1000:
+	answer += 'M';
+	num -= 1000;
+	break;
+      case num >= 900:
+	answer += 'CM';
+	num -= 900;
+	break;
+      case num >= 500:
+	answer += 'D';
+	num -= 500;
+	break;
+      case num >= 400:
+	answer += 'CD';
+	num -= 400;
+	break;
+      case num >= 100:
+	answer += 'C';
+	num -= 100;
+	break;
+      case num >= 90:
+	answer += 'XC';
+	num -= 90;
+	break;
+      case num >= 50:
+	answer += 'L';
+	num -= 50;
+	break;
+      case num >= 40:
+	answer += 'XL';
+	num -= 40;
+	break;
+      case num >= 10:
+	answer += 'X';
+	num -= 10;
+	break;
+      case num >= 9:
+	answer += 'IX';
+	num -= 9;
+	break;
+      case num >= 5:
+	answer += 'V';
+	num -= 5;
+	break;
+      case num >= 4:
+	answer += 'IV';
+	num -= 4;
+	break;
+      case num >= 1:
+	answer += 'I';
+	num -= 1;
+	break;
+      default:
+	answer = "Oops, something went wrong :/";
+	break;
+    }
+  }
+  return answer;
+};
 
-function checker(num) {
-	switch(true) {
-		case number >= 1000:
-			answer += 'M';
-		  number -= 1000;
-			break;
-		case number >= 900:
-			answer += 'CM';
-		  number -= 900;
-			break;
-		case number >= 500:
-			answer += 'D';
-		  number -= 500;
-			break;
-		case number >= 400:
-			answer += 'CD';
-		  number -= 400;
-			break;
-		case number >= 100:
-			answer += 'C';
-		  number -= 100;
-			break;
-		case number >= 90:
-			answer += 'XC';
-		  number -= 90;
-			break;
-		case number >= 50:
-			answer += 'L';
-		  number -= 50;
-			break;
-		case number >= 40:
-			answer += 'XL';
-		  number -= 40;
-			break;
-		case number >= 10:
-			answer += 'X';
-		  number -= 10;
-			break;
-		case number >= 9:
-			answer += 'IX';
-		  number -= 9;
-			break;
-		case number >= 5:
-			answer += 'V';
-		  number -= 5;
-			break;
-		case number >= 4:
-			answer += 'IV';
-		  number -= 4;
-			break;
-		case number >= 1:
-			answer += 'I';
-		  number -= 1;
-			break;
-		default:
-			answer = "Oops, something went wrong :/";
-			break;
-	}
-}
-
-console.log("Calling checker: Answer: " + answer + " Num: " + number);
-if (number >= 4000) {
-	answer = "Cannot compute numbers larger than 3999";
-	number = 0;
-}
-while (number > 0) {
-	checker(number);
-}
-console.log("Checked: Answer: " + answer + " Num: " + number);
+convertToRoman(36);
+console.log(convertToRoman(36));
