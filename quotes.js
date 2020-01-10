38ec4f2567133eeea0883eed4a08a82e3e5426aa
Shaun Gedye <sgedye88@yahoo.com.au>
2020-01-10 12:44:57 +1100

    Add files via upload

---
+++ quotes.js
@@ -0,0 +1,16 @@
+const quotes = [
+  {
+    quote: "The man who thinks he can and the man who thinks he can't are both right. Which one are you?",
+    author: "Henry Ford"
+  },
+  {
+    quote: "Care about what other people think and you will always be their prisoner.",
+    author: "Lau Tzu"     
+  },
+  {
+    quote: "It is the mark of an educated mind to be able to entertain a thought without accepting it.",
+    author: "Aristotle"
+  }
+]
+
+export default quotes
